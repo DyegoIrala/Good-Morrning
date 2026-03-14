a0395d51c9117cff011badcb8e9c22d512e45431
Dyrala <diego_irala1198@outlook.com>
2026-03-13 20:21:59 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,275 @@
+//Messages
+const totalMessages = {
+  Camilo: ["Hoy es un dia bello para Programar", "Mensajito nuevo"],
+  Diego: ["Hoy es un dia lindo para ver One Piece", "Mensajito nuevo"],
+};
+
+//This is the integrants
+const integrants = [
+  {
+    id: 1,
+    name: "Diego Irala",
+    password: "DIRALA",
+    messages: totalMessages.Diego,
+    profileImage: "img/Diego.jpg",
+    description: "Creador de la WEB, Ing. Sistemas, Senior Fronted.",
+  },
+  {
+    id: 2,
+    name: "Camilo",
+    password: "Astaaa",
+    messages: totalMessages.Camilo,
+    profileImage: "img/Camilo.jpg",
+    description:
+      "Ingeniero Casado, el primero del grupo, gozu en correr, no sabe Cloud",
+  },
+  {
+    id: 3,
+    name: "Edgar Loayza",
+    password: "Astaaa",
+    messages: ["Buenos dias gentita", "Nuevos"],
+    profileImage: "img/Edgar.jpg",
+    description:
+      "Le gusta columpiarse, Ingeniero Industrial de amplio recorrido, amante de las bicicletas.",
+  },
+  {
+    id: 4,
+    name: "Javier Exeni",
+    password: "Astaaa",
+    messages: ["Buenos dias gentita", "Nuevos"],
+    profileImage: "img/Javicho.jpeg",
+    description:
+      "Gran Persona cuando quiere, y cuando no quiere tambien, Ing. en Sistemas, Senior. Javpincho",
+  },
+  {
+    id: 5,
+    name: "David Gemio",
+    password: "Astaaa",
+    messages: ["Buenos dias gentita", "Nuevos"],
+    profileImage: "img/David.jpg",
+    description: "Ing. en Datos, le gusta emputarse, extraña jugar Counter",
+  },
+  {
+    id: 6,
+    name: "Alvaro Villavicencio",
+    password: "Astaaa",
+    messages: ["Buenos dias gentita", "Nuevos"],
+    profileImage: "img/Villa.jpg",
+    description:
+      "Ingeniero, filantropo, Deportista, Dotero, Fortnitero, cada faceta es una personalidad.",
+  },
+  {
+    id: 7,
+    name: "Daniel Barrios",
+    password: "Astaaa",
+    messages: ["Buenos dias gentita", "Nuevos"],
+    profileImage: "img/Danfer.jpg",
+    description: "Hey Thorfin, gozu, tiene su fabrica. Ing. Industrial",
+  },
+  {
+    id: 8,
+    name: "Javier Iquize",
+    password: "Astaaa",
+    messages: ["Buenos dias gentita", "Nuevos"],
+    profileImage: "img/Javiame.jpeg",
+    description: "10k mmr, referencia sexual",
+  },
+  {
+    id: 9,
+    name: "Nicolas Fernández (Ednick) ",
+    password: "Astaaa",
+    messages: ["Buenos dias gentita", "Nuevos"],
+    profileImage: "img/Ednick.jpg",
+    description:
+      "Referente de paz y felicidad, gran persona, ingeniero Senior, Vio One Piece, nada mas que decir <3",
+  },
+];
+
+const weekMessages = [
+  [
+    1,
+    "Alvaro Villavicencio",
+    integrants[1].profileImage,
+    "Mensaje de Villa",
+    "Sunday",
+  ],
+  [2, "Camilo", integrants[5].profileImage, "Camilo saluda", "Monday"],
+  [3, "Daniel Barrios", integrants[6].profileImage, "Saluda pe", "Tusday"],
+  [
+    4,
+    "David Gemio",
+    integrants[5].profileImage,
+    "Saluda pe soy manito",
+    "Wednesday",
+  ],
+  [
+    5,
+    "Diego Irala",
+    integrants[0].profileImage,
+    "Buenos dias Gentita",
+    "Thursday",
+  ],
+  [6, "Javier Iquize", integrants[7].profileImage, "Dota?", "Friday"],
+  [7, "Javier Exeni", integrants[3].profileImage, "VIVA LA VIDA", "Saturday"],
+];
+
+//NavBar
+const songButton = document.querySelector("#song");
+const music = document.getElementById("music");
+
+if (songButton && music) {
+  songButton.addEventListener("click", () => {
+    if (music.paused) {
+      music.play();
+    } else {
+      music.pause();
+    }
+  });
+}
+
+//MESSAGES FOR TODAY
+const dayForMessage = new Date().getDay();
+
+const manualIntegrant = weekMessages[dayForMessage][1];
+const manualMessage = weekMessages[dayForMessage][3];
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const order = [];
+const participantOrder = [];
+let count = 0;
+
+while (count < 7) {
+  const randomNum = Math.floor(Math.random() * integrants.length);
+  const randomName = integrants[randomNum].name;
+  const participantId = integrants[randomNum].id;
+
+  if (!order.includes(randomName)) {
+    order.push(randomName);
+
+    participantOrder.push({
+      position: count + 1,
+      name: randomName,
+      day: days[count],
+      id: participantId,
+    });
+
+    count++;
+  }
+}
+console.log(participantOrder);
+
+//Day
+
+const dayToday = days[new Date().getDay()];
+
+//TODAY MESSAGE
+const currentDay = document.querySelector(".current-day");
+const todayIntegrant = document.querySelector(".today-name");
+const imageToday = document.querySelector(".imagen-profile");
+const todaySpecial = document.querySelector(".special-greeting");
+currentDay.textContent = dayToday;
+todayIntegrant.textContent = manualIntegrant;
+todaySpecial.textContent = manualMessage;
+imageToday.src = weekMessages[dayForMessage][2];
+
+//ALEATORY MESSAGE
+//Integrants
+const allIntegrants = document.querySelector(".integrants");
+const liAllIntegrats = document.createElement("ul");
+liAllIntegrats.className = "ul-All-Integrants";
+allIntegrants.appendChild(liAllIntegrats);
+integrants.forEach((integrant) => {
+  const individualIntegrant = document.createElement("li");
+  const individualImage = document.createElement("img");
+  const individualName = document.createElement("h3");
+  const individualDescription = document.createElement("p");
+
+  individualImage.classList.add("individual-image-from-list");
+  individualName.classList.add("individual-name-from-list");
+
+  individualImage.src = integrant.profileImage;
+  individualName.textContent = integrant.name;
+  individualDescription.textContent = integrant.description;
+
+  //Classs
+  individualIntegrant.classList.add("card-container-integrant");
+  //add
+  individualIntegrant.appendChild(individualImage);
+  individualIntegrant.appendChild(individualName);
+  individualIntegrant.appendChild(individualDescription);
+
+  liAllIntegrats.appendChild(individualIntegrant);
+});
+
+//Aleatory Message
+const randomMessage = document.querySelector(".aleatory-message");
+let countDelete = 0;
+setInterval(() => {
+  countDelete++;
+  if (countDelete >= 1) {
+    randomMessage.innerHTML = "";
+    countDelete = 0;
+  }
+
+  const completeMessage = document.createElement("p");
+  const imageAleatoryProfile = document.createElement("img");
+
+  const integrant = integrants[Math.floor(Math.random() * integrants.length)];
+
+  imageAleatoryProfile.src = integrant.profileImage;
+  imageAleatoryProfile.classList.add("profile-random-img");
+  completeMessage.classList.add("random-description");
+
+  const message =
+    integrant.messages[Math.floor(Math.random() * integrant.messages.length)];
+
+  completeMessage.textContent = `${integrant.name}: ${message}`;
+
+  randomMessage.appendChild(imageAleatoryProfile);
+  randomMessage.appendChild(completeMessage);
+}, 2200);
+
+//Aleatory participant
+
+// Participants Order manual code for participants 1 time per Week
+const orderForParticipants = document.querySelector(".participants-order");
+const fromLogAleatory = [
+  [1, "Alvaro Villavicencio", "Monday"],
+  [2, "Camilo", "Thusday"],
+  [3, "Daniel Barrios", "Wednesday"],
+  [4, "David Gemio", "Thursday"],
+  [5, "Diego Irala", "Friday"],
+  [6, "Javier Iquize", "Saturday"],
+  [7, "Javier Exeni", "Sunday"],
+  [8, "ACTUALIZAR LISTA", "NO DAY"],
+];
+
+const olAleatoryIntegrants = document.createElement("ol");
+orderForParticipants.appendChild(olAleatoryIntegrants);
+
+fromLogAleatory.forEach((integrant) => {
+  const liParticipant = document.createElement("li");
+  liParticipant.className = "Participant";
+  liParticipant.textContent = `${integrant[1]} - - - - ${integrant[2]}`;
+  olAleatoryIntegrants.appendChild(liParticipant);
+});
+
+//Tomorrow
+const tomorrowContainer = document.querySelector(
+  ".tomorrow-participant-container",
+);
+
+const tomorrowName = fromLogAleatory[new Date().getDay() + 1][1];
+const day = fromLogAleatory[new Date().getDay() + 1][2];
+const participantTomorrow = document.createElement("p");
+participantTomorrow.className = "participant-tomorrow";
+participantTomorrow.textContent = `${tomorrowName}, tomorrow is your Turn, ${day}`;
+tomorrowContainer.appendChild(participantTomorrow);
